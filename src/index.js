051d5267292d60ec39af0389085f8dc685654732
manaraph <manaraph225@gmail.com>
2023-04-29 05:45:30 +0200

    Setup basic server with connection to mongo DB

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// const redis = require('redis');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/bank_app', {
+  // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection
+  .once('open', () => console.log('connected to database'))
+  .on('error', (err) => console.log('connection to database failed!!', err));
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
