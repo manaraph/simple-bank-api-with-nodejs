7bb3114c7c80da51189abc87411f1a03621dce45
manaraph <manaraph225@gmail.com>
2023-05-01 03:50:09 +0200

    Implement authentication and authorization for users based on roles

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../../controllers/user.controller');
+const { authorizeAdmin, authenticateUser } = require('../../services/auth.service');
 
 const router = express.Router();
 
-router.get('/users/', userController.getUsers);
-router.get('/user/:userId', userController.getUser);
+router.get('/users/', authorizeAdmin, userController.getUsers);
+router.get('/user/:userId', authenticateUser, userController.getUser);
 
 module.exports = router;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,64 @@
 const jwt = require('jsonwebtoken');
+const httpStatus = require('http-status');
+const { APIResponse, APIFatalResponse } = require('../utils/response');
 
 const secret = process.env.JWT_SECRET || 'secret';
 
 const issueToken = (payload) => jwt.sign(payload, secret);
 
+const authenticateUser = (req, res, next) => {
+  const bearerToken = req.headers['authorization'];
+  if (typeof bearerToken !== 'undefined') {
+    jwt.verify(bearerToken, secret, (err) => {
+      if (err) {
+        const data = {
+          message: 'Access denied',
+          err,
+        };
+        return APIResponse(res, data, httpStatus.FORBIDDEN);
+      } else {
+        next();
+      }
+    });
+  } else {
+    const data = {
+      message: 'Access denied',
+    };
+    return APIResponse(res, data, httpStatus.FORBIDDEN);
+  }
+};
+
+const authorizeAdmin = (req, res, next) => {
+  const bearerToken = req.headers['authorization'];
+  if (typeof bearerToken !== 'undefined') {
+    jwt.verify(bearerToken, secret, (err, data) => {
+      if (err) {
+        const data = {
+          message: 'Access denied',
+          err,
+        };
+        return APIResponse(res, data, httpStatus.FORBIDDEN);
+      } else {
+        if (data.user.role === 'ADMIN') {
+          next();
+        } else {
+          const data = {
+            message: 'Access denied',
+          };
+          return APIResponse(res, data, httpStatus.FORBIDDEN);
+        }
+      }
+    });
+  } else {
+    const data = {
+      message: 'Access denied',
+    };
+    return APIResponse(res, data, httpStatus.FORBIDDEN);
+  }
+};
+
 module.exports = {
   issueToken,
+  authenticateUser,
+  authorizeAdmin,
 };
